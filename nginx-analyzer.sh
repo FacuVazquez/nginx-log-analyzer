50ed76b85502fd8d47c95b441db3c5d23e817db6
Facundo Vazquez <facundofvazquez@gmail.com>
2025-05-09 02:36:31 -0300

    output formatting

--- nginx-analyzer.sh
+++ nginx-analyzer.sh
@@ -15,19 +15,19 @@ if [ ! -f "$LOG_FILE" ]; then
 fi
 
 echo "Top 5 IP addresses with the most requests:"
-awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 5
+awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 5 | awk '{print $2 " - " $1 " requests"}'
 echo ""
 
 echo "Top 5 most requested paths:"
-awk '{print $7}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 5
+awk '{print $7}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 5 | awk '{print $2 " - " $1 " requests"}'
 echo ""
 
 echo "Top 5 response status codes:"
-awk '{print $9}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 5
+awk '{print $9}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 5 | awk '{print $2 " - " $1 " requests"}'
 echo ""
 
 echo "Top 5 user agents:"
-awk -F\" '{print $6}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 5
+awk -F\" '{print $6}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 5 | awk '{print substr($0, index($0,$2)) " - " $1 " requests"}'
 
 # Extracts the IP (first column).
 # Then sort | uniq -c | sort -nr | head -n 5:
